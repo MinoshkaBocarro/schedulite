170636b7ef3df591f59b6e349d6f473e0798c4e0
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-11-21 01:46:16 +1100

    Add react-toastify

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -1,3 +1,4 @@
+import { ToastContainer, Bounce } from "react-toastify";
 import { Outlet } from "react-router-dom";
 
 // Component Imports
@@ -7,6 +8,19 @@ import Header from "./Header";
 function Layout({ user }) {
 	return (
 		<div id="app">
+			<ToastContainer
+				position="top-right"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="dark"
+				transition={Bounce}
+			/>
 			<Header id="header" user={user} />
 			<div id="app-content">
 				<Outlet />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
+// Global styling
+import "react-toastify/dist/ReactToastify.css";
+import "./scss/main.scss";
+
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<App />
