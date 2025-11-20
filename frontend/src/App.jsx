652ad1f91d3655cd4bafda8c978eb81f086eb04a
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-11-21 00:57:56 +1100

    Set up frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+// React Imports
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+	return (
+		<div className="app">
+			<BrowserRouter></BrowserRouter>
+		</div>
+	);
+}
+
+export default App;
