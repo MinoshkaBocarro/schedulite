bdfdab4ab3ab268409ba7b1c2bd690a469f02a6e
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-11-20 18:06:46 +1100

    Add validateUser to user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -36,4 +36,22 @@ const userSchema = new Schema(
 
 const User = mongoose.model("User", userSchema);
 
+function validateUser(user) {
+	const schema = Joi.object({
+		username: Joi.string().min(3).max(50).required().lowercase(),
+		password: Joi.string().min(8).max(1024).required(),
+		email: Joi.string()
+			.email({ tlds: { allow: false } })
+			.min(5)
+			.max(255)
+			.optional()
+			.allow(null),
+		firstName: Joi.string().max(255).optional().allow(null),
+		lastName: Joi.string().max(255).optional().allow(null),
+	});
+
+	return schema.validate(user);
+}
+
 module.exports.User = User;
+module.exports.validateUser = validateUser;
